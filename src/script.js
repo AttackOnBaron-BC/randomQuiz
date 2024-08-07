a029f7dc4057f69b2bbbfcbda4c1403883716261
AttackOnBaron <gianni-2008@hotmail.com>
2024-08-07 01:03:59 -0500

    Added empty html and css files for testing, updated JS and now it can generate random questions

---
+++ src/script.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+      number: 1,
+      question: "What is the capital of United Kingdom?",
+      choices: ["London", "Paris", "Nairobi"],
+      correct: 0
+    },
+    {
+      number: 2,
+      question: "How many days are there in a week?",
+      choices: ["Five", "Three", "Seven"],
+      correct: 2
+    },
+    {
+      number: 3,
+      question: "What is the closest planet to the sun?",
+      choices: ["Earth", "Mercury", "Saturn"],
+      correct: 1
+    },
+  ];
+  
+  var arr = [];
+  function randomQ(questionsLength){
+    while(arr.length < questionsLength){
+        var r = Math.floor(Math.random() * 3);
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+  }
+  
+  var qs = {};
+  function questionList(){
+      randomQ(questions.length);
+  
+      var list = [];
+      for (let a of arr) {
+        if(a < questions.length)
+          list.push(questions[a]);
+      }
+      localStorage.setItem("questionList", JSON.stringify(list));    
+  }
+  
+  function showQuestions()
+  {
+    const questions = JSON.parse(localStorage.getItem("questionList"));
+  
+    let questionText = document.querySelector("ul");  
+  
+    for (const q of questions) {
+      let li = document.createElement("li");
+      li.innerHTML = q.question;
+      questionText.appendChild(li);
+    } 
+  }
+  
+  questionList();
+  showQuestions();
