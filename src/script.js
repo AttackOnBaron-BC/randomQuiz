b170cff4ecfcbb14e0b687b16719ac182b56a7bd
evanthaproducer <evanbgehr@gmail.com>
2024-08-15 13:01:55 -0400

    added modal

--- src/script.js
+++ src/script.js
@@ -196,3 +196,27 @@ function displayResults(score, total) {
   const resultsCard = document.getElementById('quiz-results');
   resultsCard.style.display = 'block'; // Show results section
 }
+// modal js
+
+
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) =>{
+  const openBtn = document.getElementById(openButton),
+  modalContainer = document.getElementById(modalContent)
+  
+  if(openBtn && modalContainer){
+      openBtn.addEventListener('click', ()=>{
+          modalContainer.classList.add('show-modal')
+      })
+  }
+}
+showModal('open-modal','modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+  const modalContainer = document.getElementById('modal-container')
+  modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
